2a1cc06dceb4a2e50357be8d3970da57ce26f87a
Gildardo Banuelos <gbanuel3@gmail.com>
2021-06-25 16:19:33 -0500

    Customized UI

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -26,6 +26,7 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
+    
     [self.activityIndicator startAnimating];
     [self fetchMovies];
     self.refreshControl = [[UIRefreshControl alloc] init];
